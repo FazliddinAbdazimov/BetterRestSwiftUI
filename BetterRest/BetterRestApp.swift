12e9b3206fb25f4e16bfa48a3615095c11c37ab0
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-04-24 22:53:51 +0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Fazliddin Abdazimov on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
